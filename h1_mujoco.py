aa2beb0b4b51e035520d6df445a18f6e4b654101
Yutong Zhang <zhangyt2000@gmail.com>
2025-02-26 00:29:19 -0700

    Prototyping mink IK

--- h1_mujoco.py
+++ h1_mujoco.py
@@ -7,6 +7,8 @@ from mujoco_interface import MujocoInterface
 from unitree_sdk2py.core.channel import ChannelFactoryInitialize
 from unitree_sdk_interface import UnitreeSDKInterface
 
+import mink
+
 # initialize robot model
 # model = mujoco.MjModel.from_xml_path('unitree_robots/h1/scene.xml')
 model = mujoco.MjModel.from_xml_path('unitree_robots/h1/scene_pelvis_fixed.xml')
@@ -15,6 +17,22 @@ data = mujoco.MjData(model)
 mujoco_interface = MujocoInterface(model, data)
 # initialize elastic band
 # mujoco_interface.init_elastic_band()
+
+# initialize mink IK
+configuration = mink.Configuration(model)
+# set IK task
+hand = 'left_wrist'
+hand_task = mink.FrameTask(
+    frame_name=hand,
+    frame_type='site',
+    position_cost=200.0,
+    orientation_cost=0.0,
+    lm_damping=1.0
+)
+hand_mid = model.body(f'{hand}_target').mocapid[0]
+model = configuration.model
+data = configuration.data
+solver = 'quadprog'
 
 # initialize sdk
 ChannelFactoryInitialize(id=0, networkInterface='lo')
@@ -30,7 +48,8 @@ with mujoco.viewer.launch_passive(model, data, key_callback=mujoco_interface.key
     # set simulation parameters
     model.opt.timestep = 0.005
 
-    # print simulation information
+    # move hand to initial position
+    # mink.move_mocap_to_frame(model, data, f'{hand}_target', hand, 'site')
 
     # main simulation loop
     while viewer.is_running():
@@ -44,6 +63,13 @@ with mujoco.viewer.launch_passive(model, data, key_callback=mujoco_interface.key
         mujoco.mj_step(model, data)
         # sync user input
         viewer.sync()
+
+        # set target position
+        hand_task.set_target(mink.SE3.from_mocap_id(data, hand_mid))
+        vel = mink.solve_ik(configuration, [hand_task], model.opt.timestep, solver, 1e-1)
+        vel[0:16] = 0
+        vel[25] = 0
+        configuration.integrate_inplace(vel, model.opt.timestep)
 
         # print camera info so we can set at desired position
         # print(viewer.cam)
